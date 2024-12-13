0c80c12c1cf536d74f8e3bff35a57a389306b2d5
Paul Joy <paul.joy@sequoiaat.com>
2024-12-13 16:58:16 +0530

    init

---
+++ src/main/java/com/readview/book_service/book/service/BookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.readview.book_service.book.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookServiceApplication.class, args);
+	}
+
+}
